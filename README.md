b89faf5e9ba38a59a019652551b9cc548c0dee93
1crcbl <1crcbl@protonmail.com>
2021-04-27 17:31:07 +0200

    Implementation pushing children nodes and linking parents in 2LT

--- README.md
+++ README.md
@@ -3,11 +3,13 @@
 ## References
 <a id="1">[1]</a> S. Lin; B. W. Kernighan(1973). "An Effective Heuristic Algorithm for the Traveling-Salesman Problem". Operations Research. 21 (2): 498–516. [doi:10.1287/opre.21.2.498](https://pubsonline.informs.org/doi/abs/10.1287/opre.21.2.498).
 
-<a id="2">[2]</a> M. Fredman et al. (1995). "Data Structures for Traveling Salesmen". J. Algorithms 18: 432-479. [doi:10.1006/jagm.1995.1018](http://citeseer.ist.psu.edu/viewdoc/download;jsessionid=72CE6E9143B3CB461E627995CE1E419E?doi=10.1.1.49.570&rep=rep1&type=pdf)
+<a id="2">[2]</a> K. Helsgaun (2000). "An effective implementation of the Lin–Kernighan traveling salesman heuristic". European Journal of Operational Research. 126 (1): 106-130. [doi:10.1016/S0377-2217(99)00284-2](https://doi.org/10.1016/S0377-2217(99)00284-2)
 
-<a id="3">[3]</a> D. Sleator and R. Tarjan (1985). "Self-Adjusting Binary Search Trees" Journal of the ACM 32 (3): 652-686. [doi:10.1145/3828.3835](https://www.cs.cmu.edu/~sleator/papers/self-adjusting.pdf)
+<a id="3">[3]</a> M. Fredman et al. (1995). "Data Structures for Traveling Salesmen". J. Algorithms 18: 432-479. [doi:10.1006/jagm.1995.1018](http://citeseer.ist.psu.edu/viewdoc/download;jsessionid=72CE6E9143B3CB461E627995CE1E419E?doi=10.1.1.49.570&rep=rep1&type=pdf)
 
-<a id="4">[4]</a> M. Chrobak, T.Szymacha and A.Krawczyk (1990). "A data structure useful for finding Hamiltonian cycles". Theoretical Computer Science 71 (3): 419-424. [doi:10.1016/0304-3975(90)90053-K](https://www.sciencedirect.com/science/article/pii/030439759090053K)
+<a id="4">[4]</a> D. Sleator and R. Tarjan (1985). "Self-Adjusting Binary Search Trees" Journal of the ACM 32 (3): 652-686. [doi:10.1145/3828.3835](https://www.cs.cmu.edu/~sleator/papers/self-adjusting.pdf)
+
+<a id="5">[5]</a> M. Chrobak, T.Szymacha and A.Krawczyk (1990). "A data structure useful for finding Hamiltonian cycles". Theoretical Computer Science 71 (3): 419-424. [doi:10.1016/0304-3975(90)90053-K](https://www.sciencedirect.com/science/article/pii/030439759090053K)
 
 ## License
 
--- src/tour/twoleveltree.rs
+++ src/tour/twoleveltree.rs
@@ -1,70 +1,70 @@
 use std::{cell::RefCell, rc::Rc};
-use std::fmt;
 
 use crate::node::{Container, Node};
 
 use super::{Tour, Vertex};
 
-type RcChild = Rc<RefCell<InnerChild>>;
-type RcParent = Rc<RefCell<InnerParent>>;
+type RcVertex = Rc<RefCell<TltVertex>>;
+type RcParent = Rc<RefCell<ParentVertex>>;
 
 #[derive(Debug)]
 pub struct TwoLevelTree<'a> {
+    max_groupsize: usize,
     container: &'a Container,
-    groupsize: usize,
-    parents: Vec<ParentVertex>,
-    vertices: Vec<TlVertex>,
+    vertices: Vec<RcVertex>,
+    parents: Vec<RcParent>,
 }
 
 impl<'a> TwoLevelTree<'a> {
-    pub fn new(container: &'a Container, groupsize: usize) -> Self {
-        let n_segments = (container.len() / groupsize) + 1;
-        let mut parents = Vec::with_capacity(n_segments);
-        
-        for ii in 0..n_segments {
-            let p = ParentVertex::new(ii);
-            
-        }
-        
-        Self {
+    pub fn new(container: &'a Container, max_groupsize: usize) -> Self {
+        let mut result = Self {
+            max_groupsize,
             container,
-            groupsize,
-            parents,
             vertices: Vec::new(),
+            parents: Vec::new(),
+        };
+
+        result.init();
+        result
+    }
+
+    fn init(&mut self) {
+        
+
+        let n_parents = (self.container.len() / self.max_groupsize) + 1;
+        let mut parents = Vec::with_capacity(n_parents);
+        let p = create_parent(0, self.max_groupsize);
+        parents.push(p);
+
+        for ii in 1..n_parents {
+            let p = create_parent(ii, self.max_groupsize);
+            let last = parents.last().unwrap();
+            link_parents(last, &p);
+            parents.push(p);
         }
+
+        self.parents = parents;
     }
 
 }
 
 impl<'a> Tour for TwoLevelTree<'a> {
-    type Output = TlVertex;
+    type Output = TltVertex;
 
     fn get(&self, node_idx: usize) -> Option<&Self::Output> {
-        self.vertices.get(node_idx)
+        if let Some(v) = self.vertices.get(node_idx) {
+            unsafe { return v.as_ref().as_ptr().as_ref(); }
+        }
+
+        None
     }
 
-    fn next(&self, node_idx: usize) -> Option<&Self::Output> {
-        if node_idx > self.vertices.len() {
-            return None;
-        }
-
-        if let Some(idx) = self.vertices[node_idx].next_id() {
-            self.vertices.get(idx)
-        } else {
-            None
-        }
+    fn next(&self, _node_idx: usize) -> Option<&Self::Output> {
+        todo!()
     }
 
-    fn prev(&self, node_idx: usize) -> Option<&Self::Output> {
-        if node_idx > self.vertices.len() {
-            return None;
-        }
-
-        if let Some(idx) = self.vertices[node_idx].prev_id() {
-            self.vertices.get(idx)
-        } else {
-            None
-        }
+    fn prev(&self, _node_idx: usize) -> Option<&Self::Output> {
+        todo!()
     }
 
     fn between(&self, _from_idx: usize, _mid_idx: usize, _to_idx: usize) -> bool {
@@ -76,178 +76,219 @@ impl<'a> Tour for TwoLevelTree<'a> {
     }
 }
 
-#[derive(Clone, Debug)]
+#[derive(Debug)]
+pub struct TltVertex {
+    node: Node,
+    next: Option<RcVertex>,
+    prev: Option<RcVertex>,
+    parent: Option<RcParent>,
+}
+
+impl TltVertex {
+    pub fn new(node: &Node) -> Self {
+        Self {
+            node: node.clone(),
+            next: None,
+            prev: None,
+            parent: None
+        }
+    }
+}
+
+impl Vertex for TltVertex {}
+
+impl PartialEq for TltVertex {
+    fn eq(&self, other: &Self) -> bool {
+        self.node == other.node
+    }
+}
+
+#[derive(Debug)]
 struct ParentVertex {
-    inner: RcParent,
+    id: usize,
+    size: usize,
+    max_size: usize,
+    reverse: bool,
+    first: Option<RcVertex>,
+    last: Option<RcVertex>,
+    next: Option<RcParent>,
+    prev: Option<RcParent>,
 }
 
 impl ParentVertex {
-    fn new(id: usize) -> Self {
-        let inner = Rc::new(RefCell::new(InnerParent {
+    pub fn new(id: usize, max_size: usize) -> Self {
+        Self {
             id,
-            len: 0,
+            size: 0,
+            max_size,
             reverse: false,
             first: None,
             last: None,
-            prev: None,
             next: None,
-        }));
-
-        Self {
-            inner,
+            prev: None,
         }
     }
 
-    fn id(&self) -> usize {
-        self.inner.borrow().id
-    }
-
-    fn len(&self) -> usize {
-        self.inner.borrow().len
-    }
-
-    /// Returns the first element of this segment, taking into account the reversal bit flag.
-    fn first(&self) -> Option<RcChild> {
-        if self.inner.borrow().reverse {
-            self.inner.borrow().last.clone()
+    pub fn prev(&self) -> Option<&RcParent> {
+        if self.reverse {
+            self.next.as_ref()
         } else {
-            self.inner.borrow().first.clone()
+            self.prev.as_ref()
         }
     }
 
-    /// Returns the last element of this segment, taking into account the reversal bit flag
-    fn last(&self) -> Option<RcChild> {
-        if self.inner.borrow().reverse {
-            self.inner.borrow().first.clone()
+    pub fn next(&self) -> Option<&RcParent> {
+        if self.reverse {
+            self.prev.as_ref()
         } else {
-            self.inner.borrow().last.clone()
+            self.next.as_ref()
         }
     }
 
-    fn prev(&self) -> Option<RcParent> {
-        self.inner.borrow().prev.clone()
-    }
-
-    fn insert_prev(&self, new_prev: &RcParent) {
-        // TODO: check whether new_prev is an isolated node.
-        if let Some(old_prev) = self.prev() {
-            old_prev.borrow_mut().next = Some(new_prev.clone());
-            new_prev.borrow_mut().prev = Some(old_prev.clone());
-        }
-
-        new_prev.borrow_mut().next = Some(self.inner.clone());
-        self.inner.borrow_mut().prev = Some(new_prev.clone());
-    }
-
-    fn next(&self) -> Option<RcParent> {
-        self.inner.borrow().next.clone()
-    }
-
-    fn insert_next(&self, new_next: &RcParent) {
-        // TODO: check whether new_prev is an isolated node.
-        if let Some(old_next) = self.next() {
-            old_next.borrow_mut().prev = Some(new_next.clone());
-            new_next.borrow_mut().next = Some(old_next.clone());
-        }
-
-        new_next.borrow_mut().prev = Some(self.inner.clone());
-        self.inner.borrow_mut().next = Some(new_next.clone());
-    }
-
-    fn is_reverse(&self) -> bool {
-        self.inner.borrow().reverse
-    }
-}
-
-struct InnerParent {
-    /// The id of the parent node in the two-level tree.
-    id: usize,
-
-    /// Indicates how many children nodes the parent node has.
-    len: usize,
-
-    /// The flag indicates in which direction the traversal of vertices contained in this parent
-    /// node should take place.
-    ///
-    /// If the flag is set to `false`, the direction is forward. Otherwise, the vertices are
-    /// traversed in the reverse order. The default value is `false`.
-    reverse: bool,
-
-    /// The first child node of the segment, in the forward direction.
-    first: Option<RcChild>,
-
-    /// The last child node of the segment, in the forward direction.
-    last: Option<RcChild>,    
-
-    /// The reference to the the parent node preceding this segment in the tree.
-    prev: Option<RcParent>,
-
-    /// The reference to the parent node succeeding this segment in the tree.
-    next: Option<RcParent>,
-}
-
-impl fmt::Debug for InnerParent {
-    fn fmt(&self, _f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        todo!()
-    }
-}
-
-/// Representation of vertex in the two-level-tree data structure.
-#[derive(Debug, PartialEq)]
-pub struct TlVertex {
-    inner: RcChild,    
-}
-
-impl TlVertex {
-    fn new(node: &Node, parent: &ParentVertex) -> Self {
-        let inner = Rc::new(RefCell::new(InnerChild {
-            node: node.clone(),
-            next_id: None,
-            prev_id: None,
-            parent: parent.clone(),
-        }));
-
-        Self {
-            inner,
-        }
-    }
-
-    /// Returns the `id` of the next vertex in the tour, taking into account the reversal bit flag
-    /// of the parent node.
-    fn next_id(&self) -> Option<usize> {
-        if self.inner.borrow().parent.is_reverse() {
-            self.inner.borrow().prev_id
+    pub fn first(&self) -> Option<&RcVertex> {
+        if self.reverse {
+            self.last.as_ref()
         } else {
-            self.inner.borrow().next_id
+            self.first.as_ref()
         }
     }
 
-    fn prev_id(&self) -> Option<usize> {
-        if self.inner.borrow().parent.is_reverse() {
-            self.inner.borrow().prev_id
+    pub fn last(&self) -> Option<&RcVertex> {
+        if self.reverse {
+            self.first.as_ref()
         } else {
-            self.inner.borrow().next_id
+            self.last.as_ref()
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.size == 0
+    }
+}
+
+fn push_child(parent: &RcParent, child: &RcVertex) {
+    child.borrow_mut().parent = Some(parent.clone());
+
+    if parent.borrow().is_empty() {
+        parent.borrow_mut().first = Some(child.clone());
+        parent.borrow_mut().last = Some(child.clone());
+    } else {
+        if parent.borrow().reverse {
+            parent.borrow().first().unwrap().borrow_mut().prev = Some(child.clone());
+            child.borrow_mut().next = parent.borrow().first.clone();
+        } else {
+            parent.borrow().last().unwrap().borrow_mut().next = Some(child.clone());
+            child.borrow_mut().prev = parent.borrow().last.clone();
+        }
+    }
+
+    if let Some(next) = parent.borrow().next() {
+        link_children_x(Some(&child), parent.borrow().reverse, next.borrow().first(), next.borrow().reverse);
+    }
+
+    parent.borrow_mut().size += 1;
+}
+
+fn link_parents(prev: &RcParent, next: &RcParent) {
+    let (revp, revn) = (prev.borrow().reverse, next.borrow().reverse);
+    match (revp, revn) {
+        (true, true) => {
+            // reversed, reversed
+            prev.borrow_mut().prev = Some(next.clone());
+            next.borrow_mut().next = Some(prev.clone());
+        }
+        (true, false) => {
+            // reversed, forward
+            prev.borrow_mut().prev = Some(next.clone());
+            next.borrow_mut().prev = Some(prev.clone());
+        }
+        (false, true) => {
+            // forward, reversed
+            prev.borrow_mut().next = Some(next.clone());
+            next.borrow_mut().next = Some(prev.clone());
+        }
+        (false, false) => {
+            // forward, forward
+            prev.borrow_mut().next = Some(next.clone());
+            next.borrow_mut().prev = Some(prev.clone());
+        }
+    }
+
+    link_children_x(prev.borrow().last(), revp, next.borrow().first(), revn)
+}
+
+/// Link children nodes across segments.
+fn link_children_x(plc: Option<&RcVertex>, revp: bool,nfc: Option<&RcVertex>, revn: bool) {
+    // plc = previous node's last child
+    // nfc = next node's first child
+    if let (Some(p), Some(n)) = (plc, nfc) {
+        match (revp, revn) {
+            (true, true) => {
+                // reversed, reversed
+                p.borrow_mut().prev = Some(n.clone());
+                n.borrow_mut().next = Some(p.clone());
+            }
+            (true, false) => {
+                // reversed, forward
+                p.borrow_mut().prev = Some(n.clone());
+                n.borrow_mut().prev = Some(p.clone());
+            }
+            (false, true) => {
+                // forward, reversed
+                p.borrow_mut().next = Some(n.clone());
+                n.borrow_mut().prev = Some(p.clone());
+            }
+            (false, false) => {
+                // forward, forward
+                p.borrow_mut().next = Some(n.clone());
+                n.borrow_mut().prev = Some(p.clone());
+            }
         }
     }
 }
 
-impl Vertex for TlVertex {}
-
-struct InnerChild {
-    node: Node,
-    next_id: Option<usize>,
-    prev_id: Option<usize>,
-    parent: ParentVertex,
+fn create_parent(id: usize, max_size: usize) -> RcParent {
+    Rc::new(RefCell::new(
+        ParentVertex::new(id, max_size)
+    ))
 }
 
-impl fmt::Debug for InnerChild {
-    fn fmt(&self, _f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        todo!()
+fn create_vertex(node: &Node) -> RcVertex {
+    Rc::new(RefCell::new(
+        TltVertex::new(node)
+    ))
+}
+
+#[allow(dead_code, unused_imports)]
+mod tests {
+    use super::*;
+    use super::super::tests::create_container;
+
+    #[test]
+    fn test_push_child() {
+        let n_nodes = 5;
+        let container = create_container(n_nodes);
+        let vertices: Vec<RcVertex> = container.into_iter().map(|node| create_vertex(&node)).collect();
+
+        let p = create_parent(0, 3);
+        vertices.iter().for_each(|v| push_child(&p, v));
+        
+        assert_eq!(n_nodes, p.borrow().size);
     }
-}
 
-impl PartialEq for InnerChild {
-    fn eq(&self, other: &Self) -> bool {
-        self.node == other.node
+    #[test]
+    fn test_link_parents() {
+        let p1 = create_parent(0, 3);
+        let p2 = create_parent(1, 3);
+
+        // p1 -> p2
+        link_parents(&p1, &p2);
+
+        assert!(p1.borrow().prev().is_none());
+        assert!(p1.borrow().next().is_some());
+        assert!(p2.borrow().prev().is_some());
+        assert!(p2.borrow().next().is_none());
+        assert_eq!(p1.borrow().id, p2.borrow().prev().unwrap().borrow().id);
+        assert_eq!(p2.borrow().id, p1.borrow().next().unwrap().borrow().id);
     }
 }
