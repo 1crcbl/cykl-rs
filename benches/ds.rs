e452e02f7b9bc055abc7caf4668338b898ee44ff
1crcbl <1crcbl@protonmail.com>
2021-05-09 15:34:54 +0200

    Add bench flip

--- benches/ds.rs
+++ benches/ds.rs
@@ -7,6 +7,7 @@ use cykl_rs::metric::MetricKind;
 use cykl_rs::node::Container;
 use cykl_rs::tour::Array;
 use cykl_rs::tour::Tour;
+use cykl_rs::tour::Vertex;
 use cykl_rs::tour::TwoLevelTree;
 use cykl_rs::Scalar;
 
@@ -36,10 +37,10 @@ fn benchmark_predecessor(c: &mut Criterion) {
     let arr_1mil = Array::new(&container_1mil);
     let tlt_1mil = TwoLevelTree::with_default_order(&container_1mil, 100);
     c.bench_function("Array 1M Predecessor", |b| {
-        b.iter(|| arr_1mil.predecessor_at(black_box(1_000_000 - 1)))
+        b.iter(|| arr_1mil.get(black_box(1_000_000 - 1)))
     });
     c.bench_function("TLT   1M Predecessor", |b| {
-        b.iter(|| tlt_1mil.predecessor_at(black_box(1_000_000 - 1)))
+        b.iter(|| tlt_1mil.get(black_box(1_000_000 - 1)))
     });
 }
 
@@ -55,5 +56,32 @@ fn benchmark_between(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, benchmark_successor, benchmark_predecessor, benchmark_between);
+fn flip(tour: &mut impl Tour) {
+    let left = black_box(10);
+    let next_left = tour.successor_at(left).unwrap().index();
+    let right = left + 500_000;
+    let next_right = tour.successor_at(right).unwrap().index();
+    tour.flip_at(left, next_left, right, next_right);
+
+}
+
+fn benchmark_flip(c: &mut Criterion) {
+    let container_1mil = create_container(1_000_000);
+    let mut arr_1mil = Array::new(&container_1mil);
+    let mut tlt_1mil = TwoLevelTree::with_default_order(&container_1mil, 100);
+    c.bench_function("Array 1M Flip", |b| {
+        b.iter(|| flip(&mut arr_1mil))
+    });
+    c.bench_function("TLT   1M Flip", |b| {
+        b.iter(|| flip(&mut tlt_1mil))
+    });
+}
+
+criterion_group!(
+    benches,
+    benchmark_successor,
+    benchmark_predecessor,
+    benchmark_between,
+    benchmark_flip
+);
 criterion_main!(benches);
